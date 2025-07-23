7116a29a99d1b2055368ccb297b41624bc1937dd
JunedDeshmukh <jdeshmukh077@gmail.com>
2025-07-23 16:39:23 +0530

    initial express and socket server

---
+++ backend/src/index.ts
@@ -0,0 +1,32 @@
+"use client";
+console.log("backend");
+import express from "express";
+import http from "http";
+import serverConfig from "./config/serverConfig";
+
+import cors from "cors";
+import { Server } from "socket.io";
+
+const app = express();
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+       origin: "*",
+       methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log("New user connected");
+
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+});
+
+server.listen(serverConfig.PORT, () => {
+    console.log(`server started at ${serverConfig.PORT}`);
+});
